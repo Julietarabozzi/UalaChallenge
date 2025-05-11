f4c038637019966b1a0314c21aae3ddeee3bf95c
Julietarabozzi <julieta.rabozzi@gmail.com>
2025-05-11 19:09:20 -0300

    Initial Commit

---
+++ UalaChallenge/UalaChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  UalaChallengeApp.swift
+//  UalaChallenge
+//
+//  Created by Julieta Rabozzi on 11/05/2025.
+//
+
+import SwiftUI
+
+@main
+struct UalaChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ UalaChallengeTests/UalaChallengeTests.swift
@@ -0,0 +1,16 @@
+//
+//  UalaChallengeTests.swift
+//  UalaChallengeTests
+//
+//  Created by Julieta Rabozzi on 11/05/2025.
+//
+
+import Testing
+
+struct UalaChallengeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
